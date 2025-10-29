d8e50c222aef84ecca37f200f6eda608d817b449
yly1573 <lianyuanyu8@gmail.com>
2025-10-30 00:22:14 +0800

    Create brats2019.py

---
+++ data/brats2019.py
@@ -0,0 +1,297 @@
+import os
+import torch
+import numpy as np
+from glob import glob
+from torch.utils.data import Dataset
+import h5py
+import itertools
+from torch.utils.data.sampler import Sampler
+from tqdm import tqdm
+from utils import read_list, read_data, read_list_2d, read_data_2d
+
+'''class BraTS2019(Dataset):
+    """ BraTS2019 Dataset """
+
+    def __init__(self, base_dir=None, split='train', num=None, transform=None):
+        self._base_dir = base_dir
+        self.transform = transform
+        self.sample_list = []
+
+        train_path = self._base_dir+'/train.txt'
+        val_path = self._base_dir+'/val.txt'
+
+        if split == 'train':
+            with open(train_path, 'r') as f:
+                self.image_list = f.readlines()
+        elif split == 'val':
+            with open(val_path, 'r') as f:
+                self.image_list = f.readlines()
+
+        self.image_list = [item.replace('\n', '').split(",")[0] for item in self.image_list]
+        if num is not None:
+            self.image_list = self.image_list[:num]
+        print("total {} samples".format(len(self.image_list)))
+
+    def __len__(self):
+        return len(self.image_list)
+
+    def __getitem__(self, idx):
+        image_name = self.image_list[idx]
+        h5f = h5py.File(self._base_dir + "/data/{}.h5".format(image_name), 'r')
+        image = h5f['image'][:]
+        label = h5f['label'][:]
+        sample = {'image': image, 'label': label.astype(np.uint8)}
+        if self.transform:
+            sample = self.transform(sample)
+        return sample, idx'''
+        
+class BraTS2019(Dataset):
+    def __init__(self, split='train', num_cls=1, task="", repeat=None, transform=None, unlabeled=False, is_val=False, is_2d=False):
+        if is_2d:
+            self.ids_list = read_list_2d(split, task=task)
+        else:
+            self.ids_list = read_list(split, task=task)
+        self.repeat = repeat
+        self.task = task
+        if self.repeat is None:
+            self.repeat = len(self.ids_list)
+        print('total {} datas'.format(self.repeat))
+        self.transform = transform
+        self.unlabeled = unlabeled
+        self.num_cls = num_cls
+        self.is_val = is_val
+        self.is_2d = is_2d
+        if self.is_val:
+            self.data_list = {}
+            for data_id in tqdm(self.ids_list):
+                if is_2d:
+                    image, label = read_data(data_id, task=task+"_2d")
+                else:
+                    image, label = read_data(data_id, task=task)
+                self.data_list[data_id] = (image, label)
+
+    def __len__(self):
+        return self.repeat
+
+    def _get_data(self, data_id):
+        if self.is_val:
+            image, label = self.data_list[data_id]
+        else:
+            if self.is_2d:
+                image, label = read_data(data_id, task=self.task+"_2d")
+            else:
+                image, label = read_data(data_id, task=self.task)
+        return data_id, image, label
+
+
+    def __getitem__(self, index):
+        
+        index = index % len(self.ids_list)
+        data_id = self.ids_list[index]
+        _, image, label = self._get_data(data_id)
+
+        if self.unlabeled: # <-- for safety
+            label[:] = 0
+        
+        image = (image - image.min()) / (image.max() - image.min())
+        
+        image = image.astype(np.float32)
+        label = label.astype(np.int8)
+        sample = {'image': image, 'label': label}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample, index
+
+
+class CenterCrop(object):
+    def __init__(self, output_size):
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+
+        # pad the sample if necessary
+        if label.shape[0] <= self.output_size[0] or label.shape[1] <= self.output_size[1] or label.shape[2] <= \
+                self.output_size[2]:
+            pw = max((self.output_size[0] - label.shape[0]) // 2 + 3, 0)
+            ph = max((self.output_size[1] - label.shape[1]) // 2 + 3, 0)
+            pd = max((self.output_size[2] - label.shape[2]) // 2 + 3, 0)
+            image = np.pad(image, [(pw, pw), (ph, ph), (pd, pd)],
+                           mode='constant', constant_values=0)
+            label = np.pad(label, [(pw, pw), (ph, ph), (pd, pd)],
+                           mode='constant', constant_values=0)
+
+        (w, h, d) = image.shape
+
+        w1 = int(round((w - self.output_size[0]) / 2.))
+        h1 = int(round((h - self.output_size[1]) / 2.))
+        d1 = int(round((d - self.output_size[2]) / 2.))
+
+        label = label[w1:w1 + self.output_size[0], h1:h1 +
+                      self.output_size[1], d1:d1 + self.output_size[2]]
+        image = image[w1:w1 + self.output_size[0], h1:h1 +
+                      self.output_size[1], d1:d1 + self.output_size[2]]
+
+        return {'image': image, 'label': label}
+
+
+class RandomCrop(object):
+    """
+    Crop randomly the image in a sample
+    Args:
+    output_size (int): Desired output size
+    """
+
+    def __init__(self, output_size, with_sdf=False):
+        self.output_size = output_size
+        self.with_sdf = with_sdf
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+        if self.with_sdf:
+            sdf = sample['sdf']
+
+        # pad the sample if necessary
+        if label.shape[0] <= self.output_size[0] or label.shape[1] <= self.output_size[1] or label.shape[2] <= \
+                self.output_size[2]:
+            pw = max((self.output_size[0] - label.shape[0]) // 2 + 3, 0)
+            ph = max((self.output_size[1] - label.shape[1]) // 2 + 3, 0)
+            pd = max((self.output_size[2] - label.shape[2]) // 2 + 3, 0)
+            image = np.pad(image, [(pw, pw), (ph, ph), (pd, pd)],
+                           mode='constant', constant_values=0)
+            label = np.pad(label, [(pw, pw), (ph, ph), (pd, pd)],
+                           mode='constant', constant_values=0)
+            if self.with_sdf:
+                sdf = np.pad(sdf, [(pw, pw), (ph, ph), (pd, pd)],
+                             mode='constant', constant_values=0)
+
+        (w, h, d) = image.shape
+        # if np.random.uniform() > 0.33:
+        #     w1 = np.random.randint((w - self.output_size[0])//4, 3*(w - self.output_size[0])//4)
+        #     h1 = np.random.randint((h - self.output_size[1])//4, 3*(h - self.output_size[1])//4)
+        # else:
+        w1 = np.random.randint(0, w - self.output_size[0])
+        h1 = np.random.randint(0, h - self.output_size[1])
+        d1 = np.random.randint(0, d - self.output_size[2])
+
+        label = label[w1:w1 + self.output_size[0], h1:h1 +
+                      self.output_size[1], d1:d1 + self.output_size[2]]
+        image = image[w1:w1 + self.output_size[0], h1:h1 +
+                      self.output_size[1], d1:d1 + self.output_size[2]]
+        if self.with_sdf:
+            sdf = sdf[w1:w1 + self.output_size[0], h1:h1 +
+                      self.output_size[1], d1:d1 + self.output_size[2]]
+            return {'image': image, 'label': label, 'sdf': sdf}
+        else:
+            return {'image': image, 'label': label}
+
+
+class RandomRotFlip(object):
+    """
+    Crop randomly flip the dataset in a sample
+    Args:
+    output_size (int): Desired output size
+    """
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+        k = np.random.randint(0, 4)
+        image = np.rot90(image, k)
+        label = np.rot90(label, k)
+        axis = np.random.randint(0, 2)
+        image = np.flip(image, axis=axis).copy()
+        label = np.flip(label, axis=axis).copy()
+
+        return {'image': image, 'label': label}
+
+
+class RandomNoise(object):
+    def __init__(self, mu=0, sigma=0.1):
+        self.mu = mu
+        self.sigma = sigma
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+        noise = np.clip(self.sigma * np.random.randn(
+            image.shape[0], image.shape[1], image.shape[2]), -2*self.sigma, 2*self.sigma)
+        noise = noise + self.mu
+        image = image + noise
+        return {'image': image, 'label': label}
+
+
+class CreateOnehotLabel(object):
+    def __init__(self, num_classes):
+        self.num_classes = num_classes
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+        onehot_label = np.zeros(
+            (self.num_classes, label.shape[0], label.shape[1], label.shape[2]), dtype=np.float32)
+        for i in range(self.num_classes):
+            onehot_label[i, :, :, :] = (label == i).astype(np.float32)
+        return {'image': image, 'label': label, 'onehot_label': onehot_label}
+
+
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, sample):
+        image = sample['image']
+        image = image.reshape(
+            1, image.shape[0], image.shape[1], image.shape[2]).astype(np.float32)
+        if 'onehot_label' in sample:
+            return {'image': torch.from_numpy(image), 'label': torch.from_numpy(sample['label']).long(),
+                    'onehot_label': torch.from_numpy(sample['onehot_label']).long()}
+        else:
+            return {'image': torch.from_numpy(image), 'label': torch.from_numpy(sample['label']).long()}
+
+
+class TwoStreamBatchSampler(Sampler):
+    """Iterate two sets of indices
+
+    An 'epoch' is one iteration through the primary indices.
+    During the epoch, the secondary indices are iterated through
+    as many times as needed.
+    """
+
+    def __init__(self, primary_indices, secondary_indices, batch_size, secondary_batch_size):
+        self.primary_indices = primary_indices
+        self.secondary_indices = secondary_indices
+        self.secondary_batch_size = secondary_batch_size
+        self.primary_batch_size = batch_size - secondary_batch_size
+
+        assert len(self.primary_indices) >= self.primary_batch_size > 0
+        assert len(self.secondary_indices) >= self.secondary_batch_size > 0
+
+    def __iter__(self):
+        primary_iter = iterate_once(self.primary_indices)
+        secondary_iter = iterate_eternally(self.secondary_indices)
+        return (
+            primary_batch + secondary_batch
+            for (primary_batch, secondary_batch)
+            in zip(grouper(primary_iter, self.primary_batch_size),
+                   grouper(secondary_iter, self.secondary_batch_size))
+        )
+
+    def __len__(self):
+        return len(self.primary_indices) // self.primary_batch_size
+
+
+def iterate_once(iterable):
+    return np.random.permutation(iterable)
+
+
+def iterate_eternally(indices):
+    def infinite_shuffles():
+        while True:
+            yield np.random.permutation(indices)
+    return itertools.chain.from_iterable(infinite_shuffles())
+
+
+def grouper(iterable, n):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3) --> ABC DEF"
+    args = [iter(iterable)] * n
+    return zip(*args)
